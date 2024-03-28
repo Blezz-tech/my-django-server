6f7ad0bf1357798a15f42ad2fd88df672d05cd6c
blezz-tech <markus.jenya04@yandex.ru>
2024-03-28 13:26:16 +0300

    GET and POST requests. Request Exception Handlers

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+1*ezx%3m)r#w&y71j+649l6%wbc9ldpo_n66g*a1)5_)8zi0t'
+SECRET_KEY = 'django-insecure-ywt^i-jce1cq%5q4s^p0130-h9x9vdmvmge-vf8g+htkga+=71'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -8,6 +8,6 @@ register_converter(converters.FourDigitYearConverter, "year4")
 urlpatterns = [
     path('', views.index),  # http://127.0.0.1:8000
     path('cats/<int:cat_id>/', views.categories), # http://127.0.0.1:8000/cats/2/
-    path('cats/<slug:cat_slug>/', views.categories_by_slug), # http://127.0.0.1:8000/cats/fdqdq/
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),  # http://127.0.0.1:8000/cats/fdgdg/
     path("archive/<year4:year>/", views.archive),
 ]
